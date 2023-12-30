f3cb93e8e5471aee887e97bd316cbe3fc4b9e64c
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-12-30 16:17:21 +0000

    Backend block for storing statefile on azure

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=3.0.0"
     }
   }
+# Migrate/copy local state to azure blob storage container
+  backend "azurerm" {
+    resource_group_name = "my-resource-group"
+    storage_account_name = "storageaccountemreduman1"
+    container_name = "terraformstatefiles"
+    key = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
@@ -23,10 +30,17 @@ resource "azurerm_resource_group" "example" {
 
 # Define the Azure Storage Account resource
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccount1"
+  name                     = "storageaccountemreduman1"
   resource_group_name      = "my-resource-group"
   location                 = "UK South"
   account_tier             = "Standard"
   account_replication_type = "LRS"
   depends_on = [azurerm_resource_group.example]
+}
+
+#Create container on storange account
+resource "azurerm_storage_container" "terraformstatefiles" {
+  name                  = "terraformstatefiles"
+  storage_account_name  = "storageaccountemreduman1"
+  container_access_type = "private"
 }
